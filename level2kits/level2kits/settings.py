f0760e5a120d47ca2191978ed2fb6d50ddfe8a28
Kevin <kevinm1776@gmail.com>
2019-12-13 09:57:46 -0800

    Added some bootstrap.

--- level2kits/level2kits/settings.py
+++ level2kits/level2kits/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'products',
     'web',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'level2kits.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
