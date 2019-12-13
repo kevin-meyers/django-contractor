59b91ddaf155a617132c89cc9c580fd7c96c057f
Kevin <kevinm1776@gmail.com>
2019-12-13 14:09:21 -0800

    Added static file to heroku.

--- level2kits/settings.py
+++ level2kits/settings.py
@@ -15,6 +15,16 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+if os.getenv('IS_PROD'):
+    STATICFILE_DIRECTORY = 'static'
+
+else:
+    STATICFILE_DIRECTORY = 'static/assets'
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, STATICFILE_DIRECTORY)
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
