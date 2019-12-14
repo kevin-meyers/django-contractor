766f1f4a9d5bc35846fb3f03bba36367418ca20e
Kevin <kevinm1776@gmail.com>
2019-12-13 16:59:27 -0800

    Added tests.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-
+Run `python manage.py test` to run tests.
 
 
 Hosted on https://level2kits.herokuapp.com/
--- web/tests.py
+++ web/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.urls import resolve
+from django.test import TestCase, Client
+from django.http import HttpRequest
 
-# Create your tests here.
+
+from .views import Home
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, Home)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = Home(request)
+        html = response.content.decode('utf8')
+        #self.assertTrue(html.startswith("{% extends 'base.html' %}"))
+        #self.assertIn("{% include 'web/partials'", html)
+        self.assertTrue(html.endswith('</html>'))
