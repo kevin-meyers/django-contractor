2b25c60479b987d4480eb591bdc8d3e3381c4d20
Kevin <kevinm1776@gmail.com>
2019-12-13 16:23:51 -0800

    Maybe static works now

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+
+
+
 Hosted on https://level2kits.herokuapp.com/
 
-More readme to come.
--- level2kits/settings.py
+++ level2kits/settings.py
@@ -12,14 +12,20 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-if os.getenv('IS_PROD', False):
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+if bool(os.getenv('IS_PROD', False)):
     STATICFILE_DIRECTORY = 'static'
 
 else:
     STATICFILE_DIRECTORY = 'static/assets'
+    from dotenv import load_dotenv
+    load_dotenv()
 
 
 STATICFILES_DIRS = [
@@ -29,8 +35,7 @@ STATICFILES_DIRS = [
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+#086a-qb3$8kkt$w-fx((^r+#*h*5)5bgi_alk$*07w3z%bv4'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -133,5 +138,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
