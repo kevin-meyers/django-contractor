c389cd0b4c9151adbbf74c0cd4398453615ab691
Kevin <kevinm1776@gmail.com>
2019-12-13 17:03:40 -0800

    Added test to sinup.

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import resolve
+from django.http import HttpRequest
 
-# Create your tests here.
+from .views import SignupView
+
+
+class SignUpTest(TestCase):
+
+    def test_sign_up_url_resolves_to_sign_up(self):
+        found = resolve('/accounts/signup')
+        self.assertEqual(found.func, SignupView.as_view())
