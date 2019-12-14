023477007ec48bbcf1994d8c17958240129f9b1b
Kevin <kevinm1776@gmail.com>
2019-12-13 17:06:25 -0800

    Added second test, failing.

--- accounts/tests.py
+++ accounts/tests.py
@@ -10,3 +10,11 @@ class SignUpTest(TestCase):
     def test_sign_up_url_resolves_to_sign_up(self):
         found = resolve('/accounts/signup')
         self.assertEqual(found.func, SignupView.as_view())
+
+    def test_signed_in(self):
+        ''' Want to make signup auto log in user
+        Test is here but couldnt get to it in time.
+
+        '''
+        self.assert(request.user.is_authenticated)
+
