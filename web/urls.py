1bb9eb690d0260e033eff5840cefb307f73a58b3
Kevin <kevinm1776@gmail.com>
2019-12-16 10:46:25 -0800

    Set up about page.

--- web/urls.py
+++ web/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Home
+from .views import Home, About
 
 
 urlpatterns = [
     path('', Home, name='home'),
+    path('about/', About, name='about')
 ]
--- web/views.py
+++ web/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def Home(request):
     return render(request, 'web/index.html', {})
+
+def About(request):
+    return render(request, 'web/about.html', {})
