d3bddacf6706dfbd748f146c9b499192e914db9e
Kevin <kevinm1776@gmail.com>
2020-01-07 13:12:18 -0500

    Added namespacing for blog too.

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from blog.views import BlogListView
 
 app_name='blog'
 urlpatterns = [
-    path('', BlogListView.as_view(), name='list-view'),
+    path('', BlogListView.as_view(), name='list'),
 ]
