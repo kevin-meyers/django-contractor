fbec21770704b2f3f91236c9f5b76a0dce056bbd
Kevin <kevinm1776@gmail.com>
2020-01-07 14:18:16 -0500

    Added blog detail view.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from blog.views import BlogListView
+from blog.views import BlogListView, BlogDetailView
 
 app_name='blog'
 urlpatterns = [
     path('', BlogListView.as_view(), name='list'),
+    path('<str:slug>/', BlogDetailView.as_view(), name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
-
 from blog.models import Blog
 
 
@@ -15,3 +14,9 @@ class BlogListView(ListView):
         return render(request, 'blog/list.html', context)
 
 
+class BlogDetailView(DetailView):
+    model = Blog
+
+    def get(self, request, slug):
+        context = {'blog': self.get_queryset().get(slug__iexact=slug)}
+        return render(request, 'blog/detail.html', context)
